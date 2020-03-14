ed2541259a577ec61cf6c798cd38cacafe0980f0
MaxiDutra <43799837+MaxiDutra@users.noreply.github.com>
2020-03-13 22:03:13 -0300

    commit

--- enviar.php
+++ enviar.php
@@ -1,12 +1,20 @@
 <?php
-    $destino = "debris.banda@gmail.com";
-    $nombre = $_POST["txtNombre"];
-    $email = $_POST["txtEmail"];
-    $mensaje = $_POST["txtMensaje"];
-    $contenido = "Nombre: ". $nombre . "\nCorreo: " . $email . "\nMensaje: " . $mensaje;
 
-    mail($destino,"Contacto desde DébrisPag",$contenido);
-    header("Location:index.html");
+    
+        $name = $_POST['name'];
+        $subjet = $_POST['subject'];
+        $mailFrom = $_POST['mail'];
+        $message = $_POST['message'];    
+
+        $mailTo = "debris.banda@gmail.com"
+        $headers = "Correo: ".$mailFrom;
+        $txt = "Mensaje enviado desde la pagina de débris por ".$name."\n\n".$message;
+
+        mail($mailTo,$subjet,$message,$headers);
+        header("Location: index.html");
+
+   
+    
 
 
 
